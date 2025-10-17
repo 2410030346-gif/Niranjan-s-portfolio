56e84d45e3899fd750c75e52d23a1fd3d917c7b0
NIRANJAN KUMAR SINGH <2410030346@klh.edu.in>
2025-10-17 09:27:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Add smooth scroll or interactivity
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
